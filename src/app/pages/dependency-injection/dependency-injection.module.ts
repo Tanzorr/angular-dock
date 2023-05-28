ae0d808d4bd39a749a4206b1c59e4fe6a8fe21ed
alexander.tukhovsky <alexander.tukhovsky@vilmate>
2023-05-28 19:24:23 +0300

    Add depenedcy injection module

---
+++ src/app/pages/dependency-injection/dependency-injection.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DependencyInjectionComponent } from './dependency-injection.component';
+
+
+
+@NgModule({
+  declarations: [
+    DependencyInjectionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DependencyInjectionModule { }
